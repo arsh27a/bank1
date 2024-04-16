c2a93385c3373995f5af7e97870a147bbbe5c422
Kunal27N <114390811+Kunal27N@users.noreply.github.com>
2024-04-16 11:40:34 +0530

    Update Aboutus.php

---
+++ bank/Aboutus.php
@@ -0,0 +1,108 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>About Us</title>
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
+      crossorigin="anonymous"
+    />
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
+      integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA=="
+      crossorigin="anonymous"
+      referrerpolicy="no-referrer"
+    />
+    <link rel="stylesheet" href=" ./css/service.css"/>
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css"
+      rel="stylesheet"
+    />
+  </head>
+  <body>
+<?php
+ include_once('./html/nav-bar.html');
+ ?>
+ <!-- main section -->
+ <div class="container">
+    <div class="row"  style="margin-top:17vh;">
+          <div class="col-12">
+            <h3 class="text-center ">OVERVIEW</h3>
+            <p>Electronic Service Bank (ESB) is a sophisticated system designed to streamline and manage electronic financial transactions efficiently.
+                 It serves as a central hub for various banking services, enabling seamless communication between different applications, systems, and networks within a financial institution.
+                 ESB offers <strong>flexibility</strong>  in deploying new services, integrating third-party applications, and adapting to changing business requirements. It supports various protocols, data formats, and communication patterns, enabling banks to adapt quickly to market dynamics.</p>
+        </div>
+    </div>
+        <!--  -->
+      <div class="row mt-5">
+        <div class="col-12">
+            <h3 class="text-center">Our Values</h3>
+             <div class="container mt-4">
+                <img
+                  class="card-img-top"
+                  src="./images/values.jpg"
+                  alt="Card image cap"
+                />
+             </div>
+          </div>
+      </div>
+        <!--  -->
+    <div class="row mt-5">
+        <div class="col-12">
+            <h3 class="text-center">Our Vision & Mission</h3>
+            <div class="container mt-4">
+                <img
+                  class="card-img-top"
+                  src="./images/aboutUS.jpg"
+                  alt="Card image cap"
+                />
+             </div>
+        </div>
+    </div>
+          <!--  -->
+          <div class="row mt-5">
+            <h3 class="text-center">Corporate Indentify</h3>
+             <div class="col-3">
+               <img
+                  class="card-img-top" style="height:200px; width:200px; margin-top:30px; "
+                  src="./images/ESB-logo.jpeg"  
+                  alt="Card image cap"
+                />
+           </div>
+         <div class="col-9 mt-4"> 
+                <span> Corporate identity theory encompasses the principles and strategies that organizations employ to shape and communicate
+                   their distinct identity to stakeholders. It involves crafting a cohesive image through various elements such as logos, slogans,
+                  and brand messaginsg to establish a recognizable and memorable brand presence. For Electronic Service Bank (ESB), the logo plays
+                   a pivotal role in embodying its identity and values. The logo design should reflect ESB's commitment to innovation, reliability,                      and efficiency in delivering electronic banking services. Incorporating elements that symbolize technology, connectivity, and
+                 trust can resonate with customers and stakeholders, reinforcing ESB's position as a leading provider of digital banking
+                  solutions. Moreover, the logo should align with ESB's brand narrative and evoke a sense of professionalism and modernity.
+                   By strategically designing its logo within the framework of corporate identity theory, ESB can effectively communicate
+                    its brand essence and differentiate itself in the competitive banking landscape.
+                </span>
+                 </div>
+             </div>
+           </div>
+         </div>
+       </div>
+  
+  <!-- footer -->
+  <?php
+        include_once('./html/footer.html');
+    ?>
+     <!-- alert -->
+  <?php
+  include_once('./html/alert.html');
+  ?>
+   <!--  -->
+   <script
+      src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+      integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
+      crossorigin="anonymous"
+    ></script>
+  </body>
+</html>
